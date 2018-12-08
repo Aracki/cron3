d1506deb76342d1bd117b5ad1331401398bb633e
Aracki <aracki.ivan@gmail.com>
2018-12-09 00:29:53 +0100

    Add mongodump func

--- cron3.go
+++ cron3.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"github.com/robfig/cron"
 	"log"
+	"os/exec"
 )
 
 func main() {
 
 	c := cron.New()
 	c.Start()
-	if err := c.AddFunc("*/3 * * * *", func() { log.Println("Run every 3 second") }); err != nil {
-		log.Fatal(err.Error())
+	if err := c.AddFunc("*/3 * * * *", func() {
+		if err := mongoDump(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}); err != nil {
+		log.Fatal("Cannot parse cron spec:" ,err.Error())
 	}
 
 	select {}
+}
+
+func mongoDump() error {
+
+	cmd := exec.Command("mongodump")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s:%s", fmt.Sprint(err),string(out))
+	}
+
+	fmt.Println("mongodump executed")
+	return nil
 }
